60bfdfcd106e0680a74754729173b3171e52ad67
suraj480 <surajmaurya991@gmail.com>
2025-08-15 16:50:08 +0530

    adcanced pattern

---
+++ src/advancedPatterns/palindromicPattern.java
@@ -0,0 +1,27 @@
+package advancedPatterns;
+
+public class palindromicPattern {
+
+	public static void main(String[] args) {
+		int n = 5;
+		for (int i = 1; i <= n; i++) {
+			// spaces
+			for (int j = 1; j <= n - i; j++) {
+				System.out.print(" ");
+
+			}
+			// first half numbers
+			for (int j = i; j >= 1; j--) {
+				System.out.print(j);
+			}
+		//Second half numbers
+			for(int j=2;j<=i;j++) {
+				System.out.print(j);
+			}
+			System.out.println();
+		}
+		
+
+	}
+
+}
---
+++ src/advancedPatterns/solidRhombus.java
@@ -0,0 +1,21 @@
+package advancedPatterns;
+
+public class solidRhombus {
+
+	public static void main(String[] args) {
+		int n = 5;
+		for (int i = 1; i <= n; i++) {
+			int spaces = n - i;
+			for (int j = 1; j <= spaces; j++) {
+				System.out.print(" ");
+			}
+			for (int j = 1; j <= n; j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+
+		}
+
+	}
+
+}
